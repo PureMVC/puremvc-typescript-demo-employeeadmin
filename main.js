a0c8cd855328a7f13a97e258808cfcb48de8689d
tekool <github@tekool.net>
2012-11-04 14:31:56 +0100

    Only puremvc and employee admin demo are loaded as modules

--- main.js
+++ main.js
@@ -1,39 +1,19 @@
 require.config
 (
 	{
-		baseUrl: 'lib/',
+		baseUrl: '.',
 
 		paths:
 		{
-			jQueryUi: 'jquery-ui/jquery-ui-1.8.16.custom.min',
-			jqGrid: 'jqgrid/js/jquery.jqGrid.min',
-			jqGridLocale: 'jqgrid/i18n/grid.locale-en',
-
-			puremvc: 'puremvc/puremvc-typescript-standard-1.0',
-			EmployeeAdmin: '../bin/puremvc-typescript-employeeadmin-1.0'
+			puremvc: 'lib/puremvc/puremvc-typescript-standard-1.0',
+			EmployeeAdmin: 'bin/puremvc-typescript-employeeadmin-1.0'
 		},
 
 		shims:
 		{
-
-			"jqGrid":
-			{
-				deps: ["jquery","jqGridLocale"]
-			},
-
-			"jqGridLocale":
-			{
-				deps: ["jquery"]
-			},
-
-			"jQueryUi":
-			{
-				deps: ["jquery"]
-			},
-
 			"EmployeeAdmin":
 			{
-				deps: ["puremvc","jquery","jqGrid","jQueryUi"]
+				deps: ["puremvc"]
 			}
 
 		}
@@ -45,22 +25,16 @@ require
 
 	[
 		'puremvc',
-		'jqGridLocale',
-		'jqGrid',
-		'jQueryUi',
 		'EmployeeAdmin'
 	],
 
 	function
 	(
 		puremvc,
-		jqGridLocale,
-		jqGrid,
-		jQueryUi,
 		EmployeeAdmin
 	)
 	{
-		//Wait for dom ready before setting up the application.
+		//Wait for dom to be ready before setting up the application.
 		jQuery( function ()
 		{
 			var applicationFacade/*ApplicationFacade*/ = EmployeeAdmin.ApplicationFacade.getInstance();
